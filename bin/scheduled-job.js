f9ac798b88afc8bbed67eb5e88ec28fba4cb6208
Phillip Ou <pou@mit.edu>
2017-01-12 11:19:33 -0500

    refactored script to use promises.

--- bin/scheduled-job.js
+++ bin/scheduled-job.js
@@ -1,3 +1,6 @@
+var bb = require('bluebird');
+var deferred = bb.defer();
+
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/spree');
 
@@ -9,29 +12,53 @@ var time = require('../app/utils/time.js');
 
 var connection = mongoose.connection;
 
-
 var calculateAllUsersWeeklySpending = function(callback) {
+  console.log("start");
   connection.on('error', console.error.bind(console, 'connection error:'));
   var start_of_week = time.getNearestMondayBeforeDate(new Date());
   var end_of_week = time.getNearestMondayAfterDate(new Date());
   connection.once('open', function() {
-    User._bankAuthenticatedUsers(function(err, users) {
-      users.forEach(function(user){
-        Bank._getTransactions(user, start_of_week, end_of_week, function(err, transactions) {
-          var spent_this_week = TransactionUtils.calculateTotal(transactions);
-          console.log(user);
-          var weekModel = {
-                           budget : user.budget,
-                           user_id : user._id,
-                           spent : spent_this_week,
-                           start_date : start_of_week,
-                           end_date : end_of_week
-                         };
-          Week.create(weekModel, function(e, week) {
-            console.log(week);
-          });
+
+    var bankUsersPromise = function() {
+      var promise = new Promise(function(resolve,reject)  {
+        User._bankAuthenticatedUsers(function(err, users) {
+          resolve(users);
         });
       });
+
+      return promise;
+    };
+
+    var weekPromises = function(users) {
+      var promiseStack = [];
+      users.forEach(function(user){
+        var weekPromise = new Promise(function(resolve, reject) {
+          Bank._getTransactions(user, start_of_week, end_of_week, function(err, transactions) {
+            var spent_this_week = TransactionUtils.calculateTotal(transactions);
+            var weekModel = {
+                             budget : user.budget,
+                             user_id : user._id,
+                             spent : spent_this_week,
+                             start_date : start_of_week,
+                             end_date : end_of_week
+                           };
+            resolve(weekModel);
+            Week.create(weekModel, function(e, week) {
+              resolve(week)
+            });
+          });
+        });
+
+        promiseStack.push(deferred.weekPromise);
+        var weekPromises = bb.all(promiseStack);
+        return weekPromises;
+      });
+    };
+
+    var bankUsersPromise = bankUsersPromise();
+
+    bankUsersPromise.then(weekPromises).then(function() {
+      console.log("finish");
     });
   });
 }
