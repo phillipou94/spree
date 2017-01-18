d25f7dd0087fb2088d5c5a688562590f27e7921c
Phillip Ou <pou@mit.edu>
2017-01-18 14:15:16 -0500

    finished carousel.

--- app/components/EventCarousel/EventCarousel.jsx
+++ app/components/EventCarousel/EventCarousel.jsx
@@ -4,16 +4,18 @@ import CSSModules from 'react-css-modules';
 import CarouselAnimation from "./CarouselAnimation.css";
 import CarouselAnimation2 from "./CarouselAnimation2.css";
 import styles from "./EventCarousel.css";
+import time from "../../utils/time.js";
 
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Button from "../Button/Button.jsx";
 
 class BackgroundImage extends React.Component {
   render() {
     const style = {
       position: 'fixed',
-      marginTop: "40px",
-      marginLeft:"200px",
-      width: '400px',
+      marginTop: "25px",
+      marginLeft:"40px",
+      width: '550px',
       height: '300px'
     };
    return <img src={this.props.image} style={style} />;
@@ -48,16 +50,28 @@ class EventCarousel extends Component {
 
       var animation = direction === "RIGHT" ? CarouselAnimation : CarouselAnimation2;
       var that = this;
+      var date = time.formattedDateString(new Date(event.date));
+      var venueName = event.venue.name;
+      var venueAddress = event.venue.display_location;
+      var secondaryInfoText = date + " | "+venueName+" - "+venueAddress;
+      var price = event.low_price;
+
         return (
           <div>
-            <div className = {styles.leftFrame} style = {{width:"253px"}}></div>
+            <div className = {styles.leftFrame} style = {{width:"100px"}}></div>
              <ReactCSSTransitionGroup
                 transitionName={animation}
                 transitionEnterTimeout={1000}
                 transitionLeaveTimeout={1000}>
               <BackgroundImage image={image} key={image} />
               </ReactCSSTransitionGroup>
-              <div className = {styles.rightFrame} style = {{width:"600px", marginLeft:"653px"}}>
+              <div className = {styles.rightFrame} style = {{width:"800px", marginLeft:"650px"}}>
+                <div className = {styles.eventInfo}>
+                  <h1 className = {styles.eventTitle}>{event.title}</h1>
+                  <p>{secondaryInfoText}</p>
+
+                </div>
+                <Button title = {"Buy Now   $"+price} className = {styles.buyButton}/>
                 <div className = {styles.thumbnails}>
                   {events.map(function(event, index) {
                     return <EventThumbnail event = {event}
