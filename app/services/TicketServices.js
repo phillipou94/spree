56264c47eb637184fb1199a49a5a8ba2722907c4
Phillip Ou <pou@mit.edu>
2017-01-25 21:28:57 -0500

    ready for production.

--- app/services/TicketServices.js
+++ app/services/TicketServices.js
@@ -1,6 +1,6 @@
 var dev = 'http://localhost:3000/api/tickets';
 var prod = 'https://enigmatic-mesa-53717.herokuapp.com/api/tickets';
-const BASE_URL =  dev;
+const BASE_URL =  prod;
 
 var request = require('request-promise-native');
 
